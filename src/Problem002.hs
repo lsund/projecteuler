fbcbf0407773f4cdde3250d4ba7d997da41f07d5
Ludvig Sundstrom <lud.sund@gmail.com>
2021-12-11 14:15:06 +0100

    Add new solution to 2 and 9

--- src/Problem002.hs
+++ src/Problem002.hs
@@ -1,13 +1,41 @@
-
 module Problem002 where
 
-import Data.List (tail)
-
--- update 2021-11-22
 solution2 = sum $ filter even $ takeWhile (< 4000000) fibs
 
 fibs :: [Int]
 fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 collectvals :: [Int] -> Int -> [Int]
 collectvals l i = if l !! i > 4000000 then [] else l !! i : collectvals l (i + 1)
--- src/Problem009.hs
+++ src/Problem009.hs
@@ -1,13 +1,48 @@
-
 module Problem009 where
 
--- Update 22-11-2021
 solution = [a * b * c | a <- [1..498], b <- [succ a..498], c <- [succ b..498], a ** 2 + b ** 2 == c ** 2, a + b + c == 1000]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 isTriplet a b c = a ^ 2 + b ^ 2 == c ^ 2
 
 isCandidate a b c target =
-	a + b + c == target && isTriplet a b c
+  a + b + c == target && isTriplet a b c
 
 test :: Int -> Int -> Int -> Int -> [Int]
 
@@ -15,13 +50,13 @@ test target 998 999 1000 = []
 test target a 999 c = test target (a + 1) (a + 2) (a + 3)
 test target a b 1000 = test target a (b + 1) (b + 2)
 test target a b c =
-	if isCandidate a b c target then
-		[a, b, c]
-	else
-		test target a b (c + 1)
+  if isCandidate a b c target then
+                              [a, b, c]
+  else
+    test target a b (c + 1)
 
 solve :: IO ()
 solve =
-	do
-		let res = test 1000 0 1 2
-		putStrLn $ "The result is: " ++ show res
+  do
+    let res = test 1000 0 1 2
+    putStrLn $ "The result is: " ++ show res
